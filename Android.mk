75578da38f876f2c879ddda18d95e3c6feeb7ca9
Alexey Roslyakov <alexey.roslyakov@newsycat.com>
2010-08-16 19:34:05 +0400

    Added Check C unittest framework

--- Android.mk
+++ Android.mk
@@ -28,3 +28,29 @@ LOCAL_MODULE:=libcmtspeechdata
 LOCAL_PRELINK_MODULE:=false
 
 include $(BUILD_SHARED_LIBRARY)
+
+#
+# check - C unittest framework
+#
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	check/check.c         \
+	check/check_error.c   \
+	check/check_list.c    \
+	check/check_log.c     \
+	check/check_msg.c     \
+	check/check_pack.c    \
+	check/check_print.c   \
+	check/check_run.c     \
+	check/check_str.c
+##
+
+LOCAL_C_INCLUDES:= \
+	$(LOCAL_PATH)/check
+##
+
+LOCAL_MODULE:=libcheck_static
+
+include $(BUILD_STATIC_LIBRARY)
